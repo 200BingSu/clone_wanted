0d434ac7f1024433d841f5425a960d383b98a8c2
200BingSu <200bingsu@gmail.com>
2024-11-07 18:11:46 +0900

    [swiper 적용] 카드 뉴스-afer, before 예정

---
+++ js/slide-four-card.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function () {
+  const slide = new Swiper(".four-card", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    loop: true,
+  });
+});
---
+++ js/slide-three-card.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function () {
+  const slide = new Swiper(".three-card", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    loop: true,
+  });
+});
