18298a1c4c9b3301fee937165eb96d7a31ebd333
200Bingsu <200bingsu@gmail.com>
2024-11-09 19:25:14 +0900

    [CSS 파일 분리]

--- js/slide-four-card.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", function () {
-  const slide = new Swiper(".four-card", {
-    slidesPerView: 4,
-    spaceBetween: 20,
-    loop: true,
-  });
-});
--- js/slide-three-card.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", function () {
-  const slide = new Swiper(".three-card", {
-    slidesPerView: 3,
-    spaceBetween: 20,
-    loop: true,
-  });
-});
