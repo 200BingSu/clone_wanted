ebb89195de1cbc766bcaada20d7c7ea226f24c1c
200Bingsu <200bingsu@gmail.com>
2024-11-13 00:29:53 +0900

    [header js 작업중] 관련 css 마무리하고 메뉴 슬라이드 작업 예정

--- js/header-menu.js
+++ js/header-menu.js
@@ -5,17 +5,17 @@ window.addEventListener('load', function () {
   const allMenuArray = Array.from(allMenu);
   const menu = document.querySelector('.header-menu');
   allMenu.forEach(function (menu) {
-    menu.addEventListener('click', function () {
+    menu.addEventListener('mouseenter', function () {
       const selectedMenu = this;
       const otherMenu = allMenuArray.filter(function (menu) {
         return menu !== selectedMenu;
       });
       console.log('고른 메뉴: ', selectedMenu);
       console.log('나머지: ', otherMenu);
-      const menuLink = document.querySelector('.header-menu-link');
       otherMenu.forEach(function (menu) {
-        menuLink.classList.add('menu-active');
+        menu.classList.add('menu-active');
       });
+      selectedMenu.classList.remove('menu-active');
     });
   });
 });
