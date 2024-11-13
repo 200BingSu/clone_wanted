3ef8c46c7f2e45a41b4cab5e89d5f98746aebcc8
200Bingsu <200bingsu@gmail.com>
2024-11-13 22:01:40 +0900

    [서브 메뉴 작업 시작]

--- js/header-menu.js
+++ js/header-menu.js
@@ -5,17 +5,26 @@ window.addEventListener('load', function () {
   const allMenuArray = Array.from(allMenu);
   const menu = document.querySelector('.header-menu');
   allMenu.forEach(function (menu) {
-    menu.addEventListener('mouseenter', function () {
-      const selectedMenu = this;
+    menu.addEventListener('mouseenter', function (event) {
+      const selectedMenu = event.target;
       const otherMenu = allMenuArray.filter(function (menu) {
         return menu !== selectedMenu;
       });
       console.log('고른 메뉴: ', selectedMenu);
       console.log('나머지: ', otherMenu);
       otherMenu.forEach(function (menu) {
+        menu.classList.remove('header-menu');
         menu.classList.add('menu-active');
       });
       selectedMenu.classList.remove('menu-active');
+    });
+  });
+  allMenuArray.forEach(function (menu) {
+    menu.addEventListener('mouseleave', function () {
+      allMenuArray.forEach(function (menu) {
+        menu.classList.remove('menu-active');
+        menu.classList.add('header-menu');
+      });
     });
   });
 });
