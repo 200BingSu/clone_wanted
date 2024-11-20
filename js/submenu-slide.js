96e497ea6472c5b7f79f36a2952811d39ba5ed6f
200BingSu <200bingsu@gmail.com>
2024-11-20 10:15:28 +0900

    04-js 업로드

--- js/submenu-slide.js
+++ js/submenu-slide.js
@@ -1,11 +1,11 @@
-window.addEventListener('load', function () {
-  const sidemenuSwiper = new Swiper('.submenu-box', {
-    direction: 'vertical',
-    slidesPerView: 'auto',
-    freeMode: true,
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-    mousewheel: true,
-  });
-});
+// window.addEventListener('load', function () {
+//   const sidemenuSwiper = new Swiper('.submenu-box', {
+//     direction: 'vertical',
+//     slidesPerView: 'auto',
+//     freeMode: true,
+//     scrollbar: {
+//       el: '.swiper-scrollbar',
+//     },
+//     mousewheel: true,
+//   });
+// });
