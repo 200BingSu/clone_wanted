eeefc9108d53bd5fc2b8840d2a3976c27625c09d
200Bingsu <200bingsu@gmail.com>
2024-11-20 23:01:15 +0900

    더보기 메뉴 작성중

--- js/submenu.js
+++ js/submenu.js
@@ -1,8 +1,6 @@
 window.addEventListener('load', function () {
-  //채용 호버 시, 서브메뉴 높이 조절, 백그라운드 삽입
-
   //슬라이드
-  const swiper = new Swiper('.submenu-slide', {
+  const submenuSlide = new Swiper('.submenu-slide', {
     direction: 'vertical', // 세로 스크롤 활성화
     slidesPerView: 'auto',
     autoHeight: true,
@@ -12,5 +10,32 @@ window.addEventListener('load', function () {
       el: '.swiper-scrollbar',
       draggable: true, // 드래그 가능한 스크롤바
     },
+  });
+  //채용 호버 시, 서브메뉴 높이 조절, 백그라운드 삽입
+  const employ = document.querySelector('#employ');
+  const submenuBox = document.querySelector('.submenu-box');
+  const submenuBg = document.querySelector('.submenu-bg');
+
+  employ.addEventListener('mouseenter', function () {
+    submenuBox.classList.add('submenu-box-active');
+    submenuBg.classList.add('submenu-bg-active');
+  });
+  submenuBox.addEventListener('mouseleave', function () {
+    submenuBox.classList.remove('submenu-box-active');
+    submenuBg.classList.remove('submenu-bg-active');
+    submenuSlide.slideToLoop(0);
+  });
+
+  // 더보기 메뉴 애니메이션
+  const addMenuBtn = document.querySelector('#add-menu-btn');
+  const addMenuBox = document.querySelector('.add-menu-box');
+  console.log(addMenuBtn);
+  addMenuBtn.addEventListener('mouseenter', function () {
+    addMenuBox.classList.add('add-menu-box-activ');
+    submenuBg.classList.add('submenu-bg-active');
+  });
+  addMenuBox.addEventListener('mouseleave', function () {
+    submenuBox.classList.remove('submenu-box-active');
+    submenuBg.classList.remove('submenu-bg-active');
   });
 });
