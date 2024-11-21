cf814d877f6ab899ce7dd3247ec2192e690392bd
200Bingsu <200bingsu@gmail.com>
2024-11-21 23:28:53 +0900

    [json 적용] 카드뉴스 완료. latest.json 적용 예정(html 구조 grid로 바꿀 것)

--- js/swiper-slide.js
+++ js/swiper-slide.js
@@ -2,15 +2,17 @@ window.addEventListener('load', function () {
   const slideThree = new Swiper('.three-card', {
     slidesPerView: 3,
     spaceBetween: 20,
-    loop: true,
   });
-  const slideCardNews = new Swiper('.card-news-container-set-wrap', {
-    slidesPerView: 3,
-    loop: true,
+  const slideCardNews = new Swiper('.card-news-containers', {
+    slidesPerView: 2.5,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
   });
   const slideFour = new Swiper('.four-card', {
     slidesPerView: 4,
     spaceBetween: 20,
-    loop: true,
   });
 });
