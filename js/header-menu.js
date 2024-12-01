436b09fb9803cc0afe3b5c7964a44287476eb827
200Bingsu <200bingsu@gmail.com>
2024-12-01 21:46:01 +0900

    [역세권 자바스크립트 작업 중]

--- js/header-menu.js
+++ js/header-menu.js
@@ -1,18 +1,17 @@
-window.addEventListener('load', function () {
+window.addEventListener("load", function () {
   // 메뉴목록(li)
-  const menuList = document.querySelectorAll('.header-menu');
-  console.log(menuList);
+  const menuList = document.querySelectorAll(".header-menu");
 
   let selectedMenuIndex;
 
   function select(index) {
     menuList.forEach(function (menu, i) {
       if (i === index) {
-        menu.classList.add('header-menu');
-        menu.classList.remove('menu-active');
+        menu.classList.add("header-menu");
+        menu.classList.remove("menu-active");
       } else {
-        menu.classList.add('menu-active');
-        menu.classList.remove('header-menu');
+        menu.classList.add("menu-active");
+        menu.classList.remove("header-menu");
       }
     });
   }
@@ -20,19 +19,19 @@ window.addEventListener('load', function () {
   function resetMenu() {
     // 모든 메뉴에서 클래스 제거
     menuList.forEach((menu) => {
-      menu.classList.add('header-menu');
-      menu.classList.remove('menu-active');
+      menu.classList.add("header-menu");
+      menu.classList.remove("menu-active");
     });
   }
 
   // 내가 선택한 메뉴의 selectedMenuIndex를 select()함수의 기준으로 사용
   menuList.forEach(function (menu, index) {
-    menu.addEventListener('mouseenter', function () {
+    menu.addEventListener("mouseenter", function () {
       selectedMenuIndex = index;
-      console.log('고른 메뉴', selectedMenuIndex);
+      console.log("고른 메뉴", selectedMenuIndex);
       select(selectedMenuIndex);
     });
-    menu.addEventListener('mouseleave', function () {
+    menu.addEventListener("mouseleave", function () {
       resetMenu();
     });
   });
--- js/station-position-list.js
+++ js/station-position-list.js
@@ -1,4 +1,21 @@
 window.addEventListener("load", function () {
-  const select = document.querySelector(".gangnam");
-  console.log(select);
+  const selectedPosition = document.querySelector(".selected-position");
+  const stationList = document.querySelector(".station-list");
+  const station = document.querySelectorAll(".station-list li");
+  console.log(station);
+  let nowBt = false;
+  //클릭 시 리스트 창 열기
+  selectedPosition.addEventListener("click", function () {
+    const openList = () => {
+      nowBt = true;
+      stationList.classList.add("station-list-active");
+      selectedPosition.innerHTML = "<span>강남역</span><i>▴</i> ";
+    };
+    const closeList = () => {
+      nowBt = false;
+      stationList.classList.remove("station-list-active");
+      selectedPosition.innerHTML = "<span>강남역</span><i>▾</i>";
+    };
+    nowBt === false ? openList() : closeList();
+  });
 });
