7a2c1984caa24e0a3e9c60488bbbe62509d433af
200Bingsu <200bingsu@gmail.com>
2024-12-02 23:37:14 +0900

    station 자바스크립트 진행중

--- js/station-position-list.js
+++ js/station-position-list.js
@@ -2,20 +2,27 @@ window.addEventListener("load", function () {
   const selectedPosition = document.querySelector(".selected-position");
   const stationList = document.querySelector(".station-list");
   const station = document.querySelectorAll(".station-list li");
-  console.log(station);
+
   let nowBt = false;
+  let checkedStation = "강남역";
+
   //클릭 시 리스트 창 열기
   selectedPosition.addEventListener("click", function () {
     const openList = () => {
       nowBt = true;
       stationList.classList.add("station-list-active");
-      selectedPosition.innerHTML = "<span>강남역</span><i>▴</i> ";
+      selectedPosition.innerHTML = `<span>${checkedStation}</span><i>▴</i> `;
     };
     const closeList = () => {
       nowBt = false;
       stationList.classList.remove("station-list-active");
-      selectedPosition.innerHTML = "<span>강남역</span><i>▾</i>";
+      selectedPosition.innerHTML = `<span>${checkedStation}</span><i>▾</i>`;
     };
     nowBt === false ? openList() : closeList();
   });
+
+  //버튼 클릭 시, checkedStation 변수 변경
+  station.map((item, index)=>{
+    return ()
+  })
 });
