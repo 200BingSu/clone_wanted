ae3e2423167bde7d7531389ec980f273b715762a
200Bingsu <200bingsu@gmail.com>
2024-12-06 22:59:17 +0900

    자바스크립트 작업중

--- js/station-position-list.js
+++ js/station-position-list.js
@@ -1,28 +1,37 @@
 window.addEventListener("load", function () {
-  const selectedPosition = document.querySelector(".selected-position");
-  const stationList = document.querySelector(".station-list");
-  const station = document.querySelectorAll(".station-list li");
+  const stationListWrap = document.querySelector(".station-list");
+  const stationList = document.querySelectorAll(".station-list li");
+  const stationBtn = document.querySelector(".selected-position");
+  const stationBtnNameTag = document.querySelector(".selected-position span");
+  const stationBtnArrowTag = document.querySelector(".selected-position i");
 
-  let nowBt = false;
-  let checkedStation = "강남역";
+  let showList = false;
+  let checked = false;
 
-  //클릭 시 리스트 창 열기
-  selectedPosition.addEventListener("click", function () {
-    const openList = () => {
-      nowBt = true;
-      stationList.classList.add("station-list-active");
-      selectedPosition.innerHTML = `<span>${checkedStation}</span><i>▴</i> `;
-    };
-    const closeList = () => {
-      nowBt = false;
-      stationList.classList.remove("station-list-active");
-      selectedPosition.innerHTML = `<span>${checkedStation}</span><i>▾</i>`;
-    };
-    nowBt === false ? openList() : closeList();
+  const openList = () => {
+    showList = true;
+    stationListWrap.classList.add("station-list-active");
+    stationBtnArrowTag.innerHTML = "▴";
+  };
+  const closeList = () => {
+    showList = false;
+    stationListWrap.classList.remove("station-list-active");
+    stationBtnArrowTag.innerHTML = "▾";
+  };
+
+  // 버튼 클릭 시 리스트 팝업
+  stationBtn.addEventListener("click", function () {
+    showList === false ? openList() : closeList();
   });
 
-  //버튼 클릭 시, checkedStation 변수 변경
-  station.map((item, index)=>{
-    return ()
-  })
+  stationList.forEach((item, index) => {
+    const clickedStationName = item.innerHTML;
+    // 리스트 버튼 클릭 시,
+    item.addEventListener("click", () => {
+      // 1. 리스트 끄기
+      closeList();
+      // 2. 버튼 내 innerHTML 변경시키기
+      stationBtnNameTag.innerHTML = clickedStationName;
+    });
+  });
 });
