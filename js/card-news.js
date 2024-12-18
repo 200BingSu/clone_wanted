56ec135854f1a00310306a12f9cd212fbd5cf14f
200Bingsu <200bingsu@gmail.com>
2024-12-19 00:29:24 +0900

    [fix] 버튼 자바스크립트 진행 중

--- js/card-news.js
+++ js/card-news.js
@@ -29,4 +29,16 @@ window.addEventListener("load", () => {
       cardNewsContainer.innerHTML = htmlCard;
     })
     .catch((error) => {});
+  const slideCardNews = new Swiper(".card-news-containers", {
+    slidesPerView: 2.5,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".card-news-containers .next-button",
+      prevEl: ".card-news-containers .prev-button",
+    },
+  });
+  const watching = () => {
+    console.log("움직임 감지");
+  };
+  slideCardNews.on("slideMove", () => watching());
 });
